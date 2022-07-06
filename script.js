67873b2e6aa61c2ea2804e3eb3e51442b5982406
Thomas Remmer <remmert765@gmail.com>
2022-07-06 16:34:20 +0100

    fixed image class

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ pokemonArr.forEach(element => {
     if (element.types.length > 1) {
 
         container.innerHTML += `<div class="card">
-        <img src="${element.sprite}">
+        <img src="${element.sprite}" class="card__image">
         <div class="card__content">
         <h2 class="card__heading">${element.name.charAt(0).toUpperCase()}${element.name.slice(1)}</h2>
         <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>
@@ -18,7 +18,7 @@ pokemonArr.forEach(element => {
 
     else {
         container.innerHTML += `<div class="card">
-        <img src="${element.sprite}">
+        <img src="${element.sprite}" class="card__image">
         <div class="card__content">
         <h2 class="card__heading">${element.name.charAt(0).toUpperCase()}${element.name.slice(1)}</h2>
         <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} type pokemon.</p>
