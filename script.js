c465e2329507668a3542816750a4a70dd060dc63
Thomas Remmer <remmert765@gmail.com>
2022-07-06 16:17:22 +0100

    added capital letter

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ const container = document.querySelector(".card-container")
 
 pokemonArr.forEach(element => {
 
+    
+
     if (element.types.length > 1) {
         
         container.innerHTML += `<div class="card">
         <img src="${element.sprite}">
         <div class="card__content">
-        <h2 class="card__heading">${element.name}</h2>
+        <h2 class="card__heading">${element.name.charAt(0).toUpperCase()}${element.name.slice(1)}</h2>
         <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>
         </div>
         <div>`
@@ -25,7 +27,7 @@ pokemonArr.forEach(element => {
         container.innerHTML += `<div class="card">
         <img src="${element.sprite}">
         <div class="card__content">
-        <h2 class="card__heading">${element.name}</h2>
+        <h2 class="card__heading">${element.name.charAt(0).toUpperCase()}${element.name.slice(1)}</h2>
         <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} type pokemon.</p>
         </div>
         <div>`
