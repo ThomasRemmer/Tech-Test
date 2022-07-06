b5d3ef338c950198117cbe418cabb1efadcb0030
Thomas Remmer <remmert765@gmail.com>
2022-07-06 16:07:13 +0100

    task complete

--- script.js
+++ script.js
@@ -11,41 +11,30 @@ pokemonArr.forEach(element => {
 
     if (element.types.length > 1) {
         
-        container.innerHTML += `<div>
+        container.innerHTML += `<div class="card">
         <img src="${element.sprite}">
-        <h2>${element.name}</h2>
-        <p>${element.name} (#${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>`
+        <div class="card__content">
+        <h2 class="card__heading">${element.name}</h2>
+        <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} & ${element.types[1]} type pokemon.</p>
+        </div>
+        <div>`
         
     }
     
     else {
-        container.innerHTML += `<div>
+        container.innerHTML += `<div class="card">
         <img src="${element.sprite}">
-        <h2>${element.name}</h2>
-        <p>${element.name} (#${element.id}) is a ${element.types[0]} type pokemon.</p>`
+        <div class="card__content">
+        <h2 class="card__heading">${element.name}</h2>
+        <p class="card__text">${element.name} (#${element.id}) is a ${element.types[0]} type pokemon.</p>
+        </div>
+        <div>`
 
     }
     
 
 })
 
-const card = document.querySelectorAll("div")
-const text = document.querySelectorAll("p")
-const names = document.querySelectorAll("h2")
-console.log(text)
 
-card.forEach(block => {
-    block.classList.add("card")
-    block.classList.add("card__content")
-
-})
-
-text.forEach(content => {
-    content.classList.add("card__text")
-})
-
-names.forEach(name => {
-    name.classList.add("card__heading")
-})
 
 
